7ccc48b17c7667e618f33604d4ed07447cb1d833
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-08-04 11:21:23 -0400

    Commit #1

---
+++ Assets/CarController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float maxSpeed;
+    public float acceleration;
+    public float brakeSpeed;
+
+    public JointMotor2D frontWheel;
+    public JointMotor2D backWheel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
