f7e379ff5ac044583fc8efed84714d56af9e4e6e
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-08-05 15:44:25 -0400

    Commit #12

---
+++ Assets/vehicle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vehicle : MonoBehaviour
+{
+    public bool shouldPlay;
+
+    public float cellSize = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shouldPlay = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void play()
+    {
+        shouldPlay = true;
+
+        Rigidbody2D[] rbs=GetComponentsInChildren<Rigidbody2D>();
+
+        foreach (Rigidbody2D rb in rbs)
+        {
+            rb.simulated = true;
+        }
+    }
+}
